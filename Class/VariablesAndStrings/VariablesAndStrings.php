48ac0eb7fb3a9019346f03f7c63c78646692ae35
crosenquist <caleb.rosenquist@gmail.com>
2014-08-27 09:19:04 -0700

    In-class Assignment: 	Variables and Strings
    Signed-off-by: crosenquist <caleb.rosenquist@gmail.com>

---
+++ Class/VariablesAndStrings/VariablesAndStrings.php
@@ -0,0 +1,33 @@
+<?php
+	/*
+	Caleb Patrick Rosenquist
+	08/27/2014
+	CIS12
+	Purpose: To demonstrate Comments, Variables, and Strings.
+	*/
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Strings and Variables</title>
+</head>
+
+<body>
+	<?php
+		//Declare variables
+		$hours   = 40; // Worked 40 hours
+		$payRate = 9;  // Dollars per hour
+		
+		//Calculate Paycheck
+		$grossPay = $hours * $payRate;
+		
+		//Output the result
+		echo "<p>Hours worked = $hours (hrs)</p>";
+		echo '<p>Pay rate     = $$payRate</p>';
+		echo "<p>Paycheck     = $".$grossPay.'</p>';
+		
+	?>
+</body>
+</html>
