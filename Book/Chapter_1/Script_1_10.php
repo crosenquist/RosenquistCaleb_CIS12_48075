6eaeaa67b828aae2e223cdb888a6f2fcd9ca5652
crosenquist <caleb.rosenquist@gmail.com>
2014-09-03 08:37:07 -0700

    Updated Repository Directory 	• Created Specific Folders as instructed by Dr. Lehr
    Fixed PHP Code
    	• Utilizing localhost/
    	• Removing uneeded "/'s"
    
    Signed-off-by: crosenquist <caleb.rosenquist@gmail.com>

--- Book/Chapter_1/Script_1_10.php
+++ Book/Chapter_1/Script_1_10.php
@@ -23,13 +23,13 @@
 
  // Print the results using double quotation marks:
  echo "<h3>Using double quotation marks:</h3>";
- echo "<p>You are purchasing <b>$quantity</b> widget(s) at a cost of <b>$$price</b> each. With tax,
- the total comes to <b>$$total</b>. </p>\n";
+ echo "<p>You are purchasing <b>$quantity</b> widget(s) at a cost of <b>\$$price</b> each. With tax,
+ the total comes to <b>\$$total</b>. </p>\n";
 
  // Print the results using single quotation marks:
  echo '<h3>Using single quotation marks:</h3>';
- echo '<p>You are purchasing <b>$quantity</b> widget(s) at a cost of <b>$price</b> each. With tax,
- the total comes to <b>$total</b>. </p>\n';
+ echo '<p>You are purchasing <b>$quantity</b> widget(s) at a cost of <b>\$$price</b> each. With tax,
+ the total comes to <b>\$$total</b>. </p>\n';
 
  ?>
  </body>
