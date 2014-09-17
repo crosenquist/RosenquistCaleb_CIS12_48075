70e52b92405ebe3a39bf3590af9d3e2fd815a4ed
crosenquist <caleb.rosenquist@gmail.com>
2014-09-17 09:32:24 -0700

    In Class Assignment: 	• While Construct 	• DoWhile Construct
    Signed-off-by: crosenquist <caleb.rosenquist@gmail.com>

---
+++ Class/DoWhile/Trig.php
@@ -0,0 +1,63 @@
+<?php
+	/*
+	Caleb Rosenquist
+	9/15/2014
+	Purpose: First for Loop
+	*/
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Do While Table</title>
+</head>
+
+<body>
+<h1>Trig Table</h1>
+<?php
+	//Input the data from the form
+	$angStrt=$_GET['angStrt'];
+	$angEnd=$_GET['angEnd'];
+	$angInc=$_GET['angInc'];
+?>
+
+<table width="200" border="1">
+	<tr>
+    	<th>Degrees</th>
+        <th>Radians</th>
+        <th>Sine</th>
+        <th>Cosine</th>
+        <th>Tangent</th>
+    </tr>
+    
+<?php
+	//Fo - loop
+	//echo "<tr>";
+	//for($angle=$angStrt;$angle<=$angEnd;$angle+=$angInc){
+		
+	$angle=$angStrt;
+	do{
+		echo "<tr>";
+		echo "<td> $angle </td>";
+		
+		//Calculate
+		$rad     = number_format($angle*atan(1)/45,4);
+		$sine    = number_format(sin($rad),4);
+		$cosine  = number_format(cos($rad),4);
+		$tangent = number_format(tan($rad),4);
+		
+		echo "<td>$rad</td>";
+		echo "<td>$sine</td>";
+		echo "<td>$cosine</td>";
+		echo "<td>$tangent</td>";
+		echo "</tr>";
+		$angle+=$angInc;
+	}while($angle<=$angEnd);
+	
+?>
+
+</table>
+
+</body>
+</html>
